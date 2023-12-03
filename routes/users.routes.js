0e2193e28e68ffc5500f5db048bcdea92c563419
FaterYU <1042277145@qq.com>
2023-12-03 18:48:55 +0800

    fix: user findall is get

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   const users = require("../controllers/users.controller.js");
   var router = require("express").Router();
   router.post("/create", users.create);
-  router.post("/findAll", users.findAll);
+  router.get("/findAll", users.findAll);
   router.post("/findOne", users.findOne);
   router.put("/update", users.update);
   router.delete("/delete", users.delete);
